094579ac2a98ab60cfe493290a345da5ded67e73
aibd <countable@163.com>
2023-02-06 16:32:00 +0800

    uos club

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p><b>关雎</b></p>
+        <p>关关雎鸠，在河之洲。窈窕淑女，君子好逑。</p>
+        <p> 参差荇菜，左右流之。窈窕淑女，寤寐求之。</p>
+        <p>求之不得，寤寐思服。悠哉悠哉，辗转反侧。</p>
+        <p>参差荇菜，左右采之。窈窕淑女，琴瑟友之。</p>
+        <p>参差荇菜，左右芼之。窈窕淑女，钟鼓乐之。</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 5px 40px;
+}
+</style>
